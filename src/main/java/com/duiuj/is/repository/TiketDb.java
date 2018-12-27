9ab2d6ec32a238cc26f4bb2dd49149113ef2ab32
Ricky Chandra Johanes <36234742+kaelky@users.noreply.github.com>
2018-12-27 16:44:36 +0700

    Update TiketDb.java

--- src/main/java/com/duiuj/is/repository/TiketDb.java
+++ src/main/java/com/duiuj/is/repository/TiketDb.java
@@ -2,7 +2,11 @@ package com.duiuj.is.repository;
 
 import com.duiuj.is.model.TiketModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TiketDb extends JpaRepository<TiketModel, Long> {
+    TiketModel findByKodeTiket(String kodeTiket);
+    TiketModel findByNamaLengkapAndNomorHandphone(String namaLengkap, String nomorHandphone);
 
 }
