37a8557eeee7cc4c3cea1b9f73d84b9c8ae108ca
Ricky Chandra Johanes <36234742+kaelky@users.noreply.github.com>
2018-12-27 16:44:48 +0700

    Delete SiswaDb.java

--- src/main/java/com/duiuj/is/repository/SiswaDb.java
+++
@@ -1,8 +0,0 @@
-package com.duiuj.is.repository;
-
-import com.duiuj.is.model.SiswaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SiswaDb extends JpaRepository<SiswaModel, Long> {
-
-}
