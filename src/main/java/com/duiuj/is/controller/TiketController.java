d5876fb28f1fbd4c88c02ee65a533fdd6d41e227
Winda <windanyaan64@gmail.com>
2018-12-30 22:59:26 +0700

    front end create tiket

--- src/main/java/com/duiuj/is/controller/TiketController.java
+++ src/main/java/com/duiuj/is/controller/TiketController.java
@@ -45,24 +45,39 @@ public class TiketController {
         model.addAttribute("ticket", ticket);
         return "tiket-detail";
     }
-
-    @PostMapping(value="/main-event/pay")
-    public TiketModel addTiket(@RequestBody TiketModel tiket){
-        boolean flag = true;
-        while(flag){
-            String uuid = UUID.randomUUID().toString().substring(0,8).toUpperCase();
-            for(TiketModel oldTiket: tiketService.getAll()){
-                if(uuid.equals(oldTiket.getKodeTiket())){
-                    flag = true;
-                }else{
-                    flag = false;
-                    tiket.setKodeTiket(uuid);
-                }
-            }
-        }
-        return tiketService.add(tiket);
+    
+    @GetMapping(value = "/main-event/order")
+    private String order(Model model){
+        TiketModel tiket = new TiketModel();
+        model.addAttribute("tiket", tiket);
+        return "main-event_order";
     }
+    
+//    @PostMapping(value="/main-event/pay")
+//    public String addTiket(@ModelAttribute TiketModel tiket, Model model){
+//        boolean flag = true;
+//        while(flag){
+//            String uuid = UUID.randomUUID().toString().substring(0,8).toUpperCase();
+//            for(TiketModel oldTiket: tiketService.getAll()){
+//                if(uuid.equals(oldTiket.getKodeTiket())){
+//                    flag = true;
+//                }else{
+//                    flag = false;
+//                    tiket.setKodeTiket(uuid);
+//                }
+//            }
+//        }
+//        tiket.setPasswordTiket(tiket.getKodeTiket());
+//        tiketService.add(tiket);
+//        model.addAttribute("tiket", tiket);
+//        return "main-event_pay";
+//    }
 
+    @GetMapping(value="/main-event/pay")
+    public String addTiket(){
+    	return "main-event_pay";
+    }
+    
     /*@DeleteMapping(value="/admin/tickets/delete/{ticket-id}")
     public void deleteTiket(@PathVariable("ticket-id") long id){
         TiketModel tiket = tiketService.getById(id);
@@ -73,7 +88,7 @@ public class TiketController {
     public List<TiketModel> viewAllTickets(){
         return tiketService.getAll();
     }*/
-
+    
     @PutMapping(value = "/main-event/pay")
     public TiketModel updateTiketSubmit(@RequestParam(value = "ticket-id") long id,
                                     @RequestBody TiketModel tiket) {
